f510cbba4de7a11548eef10fdaef1ef2d0c43bd5
RohitRegimon <rrgmon@protonmail.com>
2020-02-16 15:44:17 +0530

    Added mobile responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+function menuToggle() {
+  const toggleBtn = document.getElementsByClassName("togglebutton")[0];
+  const nav = document.getElementsByClassName("sidebar-page-list")[0];
+  toggleBtn.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = "portfolio";
+
+  ngOnInit() {
+    menuToggle();
+  }
 }
